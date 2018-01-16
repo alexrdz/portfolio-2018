8bf1db78d1c234ce865911a394ef1e6ef8297767
Alex Rodriguez <drinkhorchata@gmail.com>
2018-01-15 19:30:33 -0600

    add about and contact sections. plus various small fixes and updates

--- js/app.js
+++ js/app.js
@@ -4,26 +4,56 @@ window.onload = function () {
 };
 
 
-var siteNavLinks = document.querySelectorAll('.site-nav a');
+//- DOM Elements
+var workLink = document.querySelector('.work-link');
+var aboutLink = document.querySelector('.about-link');
+var contactLink = document.querySelector('.contact-link');
+var aboutContent = document.querySelector('.about-content');
+var contactContent = document.querySelector('.contact-content');
 var picContent = document.querySelector('.alex-img');
 var mainContent = document.querySelector('.home__main');
 var footerContent = document.querySelector('.home__footer');
+var closeTriggers = document.querySelectorAll('.close-panel-trigger');
 
-siteNavLinks.forEach(function(link) {
+
+workLink.addEventListener('click', function (e) {
+  e.preventDefault();
+  var page = this.getAttribute('href');
+  picContent.classList.add('slide-right');
+
+
+  mainContent.classList.toggle('fade-out-up');
+  mainContent.classList.toggle('fade-in-down');
+
+  footerContent.classList.toggle('fade-out-down');
+  footerContent.classList.toggle('fade-in-up');
+
+  setTimeout(function () {
+    aboutContent.classList.remove('slide-in-left');
+    contactContent.classList.remove('slide-in-left');
+  }, 250);
+  setTimeout(function () {
+    window.location.assign(page);
+  }, 750);
+
+});
+
+
+aboutLink.addEventListener('click', function (e) {
+  e.preventDefault();
+  window.scroll(0, 0);
+  aboutContent.classList.add('slide-in-left');
+});
+
+contactLink.addEventListener('click', function (e) {
+  e.preventDefault();
+  window.scroll(0, 0);
+  contactContent.classList.add('slide-in-left');
+});
+
+closeTriggers.forEach(function (link) {
   link.addEventListener('click', function (e) {
     e.preventDefault();
-    var page = this.getAttribute('href');
-    picContent.classList.add('slide-right');
-
-    mainContent.classList.toggle('fade-out-up');
-    mainContent.classList.toggle('fade-in-down');
-
-    footerContent.classList.toggle('fade-out-down');
-    footerContent.classList.toggle('fade-in-up');
-
-    setTimeout(function () {
-      window.location.assign(page);
-    }, 750);
-
+    return this.parentNode.classList.remove('slide-in-left');
   });
-});
+})
