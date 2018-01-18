28689baf9bd121d4a4f1a13672daba3d69546666
Alex Rodriguez <drinkhorchata@gmail.com>
2018-01-17 21:06:38 -0600

    add 404 page

--- alexrdz.harp/js/app.js
+++ alexrdz.harp/js/app.js
@@ -17,47 +17,48 @@ var mainContent = document.querySelector('.home__main');
 var footerContent = document.querySelector('.home__footer');
 var closeTriggers = document.querySelectorAll('.close-panel-trigger');
 
-
-workLink.addEventListener('click', function (e) {
-  e.preventDefault();
-  var page = this.getAttribute('href');
-  picContent.classList.add('slide-right');
-
-
-  mainContent.classList.toggle('fade-out-up');
-  mainContent.classList.toggle('fade-in-down');
-
-  footerContent.classList.toggle('fade-out-down');
-  footerContent.classList.toggle('fade-in-up');
-
-  setTimeout(function () {
-    aboutContent.classList.remove('slide-in-left');
-    contactContent.classList.remove('slide-in-left');
-  }, 250);
-  setTimeout(function () {
-    window.location.assign(page);
-  }, 750);
-
-});
-
-
-aboutLink.addEventListener('click', function (e) {
-  e.preventDefault();
-  window.scroll(0, 0);
-  contactContent.classList.remove('slide-in-left');
-  aboutContent.classList.add('slide-in-left');
-});
-
-contactLink.addEventListener('click', function (e) {
-  e.preventDefault();
-  window.scroll(0, 0);
-  aboutContent.classList.remove('slide-in-left');
-  contactContent.classList.add('slide-in-left');
-});
-
-closeTriggers.forEach(function (link) {
-  link.addEventListener('click', function (e) {
+if (workLink && aboutLink && contactLink && closeTriggers) {
+  workLink.addEventListener('click', function (e) {
     e.preventDefault();
-    return this.parentNode.classList.remove('slide-in-left');
+    var page = this.getAttribute('href');
+    picContent.classList.add('slide-right');
+
+
+    mainContent.classList.toggle('fade-out-up');
+    mainContent.classList.toggle('fade-in-down');
+
+    footerContent.classList.toggle('fade-out-down');
+    footerContent.classList.toggle('fade-in-up');
+
+    setTimeout(function () {
+      aboutContent.classList.remove('slide-in-left');
+      contactContent.classList.remove('slide-in-left');
+    }, 250);
+    setTimeout(function () {
+      window.location.assign(page);
+    }, 750);
+
   });
-})
+
+
+  aboutLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scroll(0, 0);
+    contactContent.classList.remove('slide-in-left');
+    aboutContent.classList.add('slide-in-left');
+  });
+
+  contactLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scroll(0, 0);
+    aboutContent.classList.remove('slide-in-left');
+    contactContent.classList.add('slide-in-left');
+  });
+
+  closeTriggers.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      return this.parentNode.classList.remove('slide-in-left');
+    });
+  });
+}
